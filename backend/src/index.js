0ba2cb6cbb423cf04a9adddfd15dfab0e0045d60
KZTN <kakaiocesar2@gmail.com>
2020-04-13 18:08:53 -0300

    :pencil2: Fixing typos

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes.js");
 const path = require("path");
+const cors = require("cors");
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const app = express();
-const server = require("http").server(app);
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
 app.use((req, res, next) => {
   req.io = io;
@@ -19,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(
   "/files",
-  express.staitic(path.resolve(__dirname, "..", "uploads", "resized"))
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
 );
 app.use(routes);
 
